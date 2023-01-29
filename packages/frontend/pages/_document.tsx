fe995613495ac3bd0b288fef08f17bf5bef67ebf
iamstutishah <shah0794@umn.edu>
2023-01-28 21:14:15 -0600

    😮 added font family Cabin

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -5,7 +5,14 @@ import theme from "../theme";
 export default function Document(): JSX.Element {
   return (
     <Html lang={"en"}>
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
