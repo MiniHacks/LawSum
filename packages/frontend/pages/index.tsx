d1ceefdeb829facaa9f7688fc0d3c5ff501ffbf5
Brandon <brandonnguyenlam@gmail.com>
2023-01-28 20:00:30 -0600

    🐧 installed new packages + handled uploading files

--- packages/frontend/pages/index.tsx
+++ packages/frontend/pages/index.tsx
@@ -1,15 +1,62 @@
-import type { NextPage } from "next";
-import { Box, Heading } from "@chakra-ui/react";
-import PageLayout from "../components/Layout/PageLayout";
+import type { NextPage } from 'next'
+import React from "react";
+import {Box} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
-  return (
-    <PageLayout title={"geese, by minihacks"}>
-      <Box px={[5, 10]}>
-        <Heading as={"h1"}>geese</Heading>
-      </Box>
-    </PageLayout>
-  );
-};
 
-export default Home;
+    const [isLoading, setIsLoading] = React.useState(false);
+    const inputFileRef = React.useRef<HTMLInputElement | null>(null);
+
+    const handleOnClick = async (e: React.MouseEvent<HTMLInputElement>) => {
+
+        /* Prevent form from submitting by default */
+        e.preventDefault();
+
+        /* If file is not selected, then show alert message */
+        if (!inputFileRef.current?.files?.length) {
+            alert('Please, select file you want to upload');
+            return;
+        }
+
+        setIsLoading(true);
+
+        /* Add files to FormData */
+        const formData = new FormData();
+        Object.values(inputFileRef.current.files).forEach(file => {
+            formData.append('file', file);
+        })
+
+        /* Send request to our api route */
+        const response = await fetch('/api/upload', {
+            method: 'POST',
+            body: formData
+        });
+
+        const body = await response.json() as { status: 'ok' | 'fail', message: string };
+
+        alert(body.message);
+
+        if (body.status === 'ok') {
+            inputFileRef.current.value = '';
+            // Do some stuff on successfully upload
+        } else {
+            // Do some stuff on error
+        }
+
+        setIsLoading(false);
+    };
+
+    return (
+        <form>
+            <Box>
+                <input type="file" name="myfile" ref={inputFileRef}/>
+            </Box>
+            <Box>
+                <input type="submit" value="Upload" disabled={isLoading} onClick={handleOnClick} />
+                {isLoading && ` Wait, please...`}
+            </Box>
+        </form>
+    )
+}
+
+export default Home
